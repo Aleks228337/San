9fbe22cb16abc0b224d0b4b88da5b3c46e611e51
Alik <sann2017lis@mail.ru>
2019-02-26 00:01:47 +0300

    last commit

--- 1 day/Program.cs
+++ 1 day/Program.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace _1day
 {
-   
+    class Monitor
+    {
+        private string firm;
+        public string Firm { get; set; }
+        private string type;
+        public string Type { get; set; }
+        private float diagonal;
+        public float Diagonal { get; set; }
+
+
+        public void print()
+        {
+            Console.WriteLine("Фирма:" + Firm + "Тип:" + Type + "Диагональ:" + Diagonal);
+        }
+
+        public void vrite()
+        {
+            Console.WriteLine("Введите фирму");
+            firm = Console.ReadLine();
+            Console.WriteLine("Введите тип");
+            type = Console.ReadLine();
+            Console.WriteLine("Введите диагональ");
+            diagonal = int.Parse(Console.ReadLine());
+        }
+        public Monitor()
+        {
+            Firm = "LG ";
+            Type = "ЖК ";
+            Diagonal = 19;
+        }
+
+        public Monitor(string Firm, string Type, float Diagonal)
+        {
+           this.Firm = Firm;
+            this.Type = Type;
+            this.Diagonal = Diagonal;
+        }
+
+    }
 
     class Program
     {
@@ -14,7 +52,9 @@ namespace _1day
 
         static void Main(string[] args)
         {
-           
+            Monitor name = new Monitor();
+            name.print();
+            Console.ReadKey();
         }
     }
 }
